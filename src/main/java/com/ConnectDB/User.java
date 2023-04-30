53b599bab20fd6cda9b1faaceea2c8164ea79196
unknown <amineelokri10@gmail.com>
2023-04-30 16:18:24 +0100

    ConnectDB

---
+++ src/main/java/com/ConnectDB/User.java
@@ -0,0 +1,48 @@
+package com.ConnectDB;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "mail")
+    private String mail;
+
+    public User() {
+    }
+
+    public User(String name, String mail) {
+        this.name = name;
+        this.mail = mail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
